12322dc77a9eda42dfcec40201502d5f3e846bd1
Robert Kuncewicz <rkuncewicz@gmail.com>
2014-09-01 19:35:45 +0000

    Committing first few files of MEAN stack

---
+++ public/js/services/MainService.js
@@ -0,0 +1,21 @@
+angular.module('MainService', []).factory('Main', ['$http', function($http) {
+
+    return {
+        // call to get all nerds
+        get : function() {
+            return $http.get('/api/geeks');
+        },
+
+        // call to POST and create a new geek
+        create : function(geekData) {
+            return $http.post('/api/geeks', geekData);
+        },
+
+        // call to DELETE a geek
+        delete : function(id) {
+            return $http.delete('/api/geeks/' + id);
+        }
+    }
+    
+}]);
+
